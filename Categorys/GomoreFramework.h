439a1dbd87179782deb402fe308abea19a4201a3
曹云霄 <chinacgcgcg@163.com>
2017-02-06 15:32:22 +0800

    release

--- Categorys/GomoreFramework.h
+++ Categorys/GomoreFramework.h
@@ -9,24 +9,24 @@
 #ifndef GomoreFramework_h
 #define GomoreFramework_h
 
-
-#import <NSDictionary+Category.H>
-#import <NSArray+Category.H>
-#import <NSBundle+Category.H>
-#import <NSDate+Category.H>
-#import <NSObject+Category.H>
-#import <NSString+Category.H>
-#import <UIBarButtonItem+Category.H>
-#import <UIButton+Category.H>
-#import <UIColor+Category.H>
-#import <UIImage+Category.H>
-#import <UIImageView+AnimationView.H>
-#import <UIView+Category.H>
-#import <UIViewController+Category.H>
-#import <UIViewController+DDPopUpViewController.H>
-#import <GomoreHUDProgressView.H>
-#import <CalculateHelper.H>
-#import <XYDownMenuViewController.H>
+#import "CXHeaderFile"
+#import "NSDictionary+Category.H"
+#import "NSArray+Category.H"
+#import "NSBundle+Category.H"
+#import "NSDate+Category.H"
+#import "NSObject+Category.H"
+#import "NSString+Category.H"
+#import "UIBarButtonItem+Category.H"
+#import "UIButton+Category.H"
+#import "UIColor+Category.H"
+#import "UIImage+Category.H"
+#import "UIImageView+AnimationView.H"
+#import "UIView+Category.H"
+#import "UIViewController+Category.H"
+#import "UIViewController+DDPopUpViewController.H"
+#import "GomoreHUDProgressView.H"
+#import "CalculateHelper.H"
+#import "XYDownMenuViewController.H"
 
 
 
--- GomoreFramework.podspec
+++ GomoreFramework.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "GomoreFramework"
-  s.version      = "0.0.1"
+  s.version      = "1.0.0"
   s.summary      = "上海勾芒科技有限公司"
   s.description  = <<-DESC
                    上海勾芒科技有限公司 All rights reserved
@@ -9,7 +9,7 @@ Pod::Spec.new do |s|
   s.homepage     = "https://github.com/caoyunxiao/GomoreFramework"
   s.license      = { :type => 'MIT', :file => 'LICENSE' }
   s.author       = { 'Matej gomoretech' => '3179793140@qq.com' }
-  s.source       = { :git => "https://github.com/caoyunxiao/GomoreFramework.git", :tag => "0.0.1" }
+  s.source       = { :git => "https://github.com/caoyunxiao/GomoreFramework.git", :tag => "1.0.0" }
   s.ios.deployment_target = '8.0'
   s.tvos.deployment_target = '9.0'
   s.source_files = "**/*.{h,m}"
